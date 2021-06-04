280c395f00ad4832dd28093aa53e0937d4d45df9
MattiaR <rovitomattia91@gmail.com>
2021-06-04 14:10:36 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Creiamo un finto biglietto del treno con:
+
+// Step 1 Inserire il nome passeggero
+
+// Step 2 Inserire il codice treno (numero casuale tra 90000 e 100000 escluso)
+
+// Step 3 Inserire il numero della carrozza (numero casuale tra 1 e 9 incluso)
+
+// Step 4 Inserire il prezzo calcolato in base al vecchio esercizio
+
+// Step 5 Categoria selezionata dall'utente (minorenne/maggiorenne/over65)
+
+// Step 6 Al click su "Genera" appare la schermata con le caratteristiche del biglietto
+
+// Step 7 Al click su "Annulla" dobbiamo ripulire il form e far sparire la sezione che calcola il prezzo del biglietto.
+
+// Step 8 Bonus: aggiungete qualche controllo sui dati di input!
